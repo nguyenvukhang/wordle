f810da436ac8d70b23a13806f6aab7ab52993cfa
nguyenvukhang <brew4k@gmail.com>
2023-03-30 21:03:24 +0800

    feat: update words generator

--- rust/src/words/mod.rs
+++ rust/src/words/mod.rs
@@ -5,14 +5,9 @@ mod guesses;
 
 pub use answers::ANSWERS;
 pub use guesses::GUESSES;
+
 pub fn build(list: &[&[u8; 5]]) -> Vec<[u8; 5]> {
-    list.into_iter()
-        .map(|v| {
-            let mut r = [0, 0, 0, 0, 0];
-            (0..5).for_each(|i| r[i] = v[i]);
-            r
-        })
-        .collect()
+    list.into_iter().map(|v| *v.to_owned()).collect()
 }
 
 #[allow(unused)]
--- rust/words/generate.js
+++ rust/words/generate.js
@@ -1,14 +1,25 @@
-const { readFileSync, writeFileSync } = require("fs");
+const { spawnSync } = require("child_process");
+const { readFileSync, writeFileSync, rmSync } = require("fs");
+const { join, relative } = require("path");
+
+const TARGET_DIR = join(__dirname, "../src/words");
 
 /**
  * @param {string} filename
  */
 function build(filename) {
   const words = readFileSync(filename, "utf8").trim().split("\n");
-  const len = words.length;
-  const pre = `pub const ${filename.toUpperCase()}: [&[u8; 5]; ${len}] = [`;
-  const content = pre + words.map((v) => `b"${v}"`).join(",") + "];";
-  writeFileSync(filename + ".rs", content);
+  const content =
+    `pub const ${filename.toUpperCase()}: [&[u8; 5]; ${words.length}] = [` +
+    words.map((v) => `b"${v}"`).join(",") +
+    "];";
+  const filepath = join(TARGET_DIR, filename + ".rs");
+  rmSync(filepath, { recursive: true, force: true });
+  writeFileSync(filepath, content);
+  console.log(`Generated ${relative(__dirname, filepath)}!`);
+  try {
+    spawnSync("rustfmt", [filepath], { cwd: TARGET_DIR });
+  } catch {}
 }
 
 build("guesses");
