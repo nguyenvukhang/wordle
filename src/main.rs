2889ede278343c82ddeb862ba2c2b99cb1374800
nguyenvukhang <brew4k@gmail.com>
2023-03-31 13:27:32 +0800

    feat: refactor into a struct that manages runs

--- src/main.rs
+++ src/main.rs
@@ -4,72 +4,85 @@ mod util;
 mod words;
 
 use node::Node;
-use std::time::Instant;
+use std::time::{Duration, Instant};
 use types::Word;
 use util::st;
 use util::{outcome, suggest};
 use words::GUESSES;
 
-fn solve(answers: &[&Word], index: usize, mut graph: &mut Node) -> (u32, Word) {
-    let mut remaining_ans = words::build(answers);
-    let mut tries = 0;
-    let mut guesses = vec![];
+struct Wordle {
+    guesses: Vec<Word>,
+    answers: Vec<Word>,
+}
 
-    let fixed_answer = answers[index];
-
-    while remaining_ans.len() > 1 {
-        let guess = match graph.guess {
-            Some(v) => v,
-            None => suggest(&GUESSES, &remaining_ans).to_owned(),
-        };
-
-        // everytime an outcome is generated, increment the tries by one
-        let out = outcome(&guess, &fixed_answer);
-        tries += 1;
-
-        // direct hit on correct answer
-        if out == 242 {
-            return (tries, guess);
-        }
-
-        // shrink answer space
-        remaining_ans.retain(|&answer| outcome(&guess, &answer) == out);
-
-        // save past decisions
-        graph = graph.push(guess, out);
-        guesses.push(st(&guess).to_string());
+impl Wordle {
+    pub fn new(guesses: Vec<Word>, answers: Vec<Word>) -> Self {
+        Self { guesses, answers }
     }
 
-    // At this point, remaining_ans should have one left inside
-    // This takes one last try to get it
-    //
-    debug_assert!(remaining_ans.len() == 1);
-    (tries + 1, remaining_ans[0])
+    pub fn solve_one(&self, answer_index: usize, mut graph: &mut Node) -> (u32, Word) {
+        let mut remaining_ans = self.answers.clone();
+        let mut tries = 0;
+        let mut guesses = vec![];
+
+        let fixed_answer = self.answers[answer_index];
+
+        while remaining_ans.len() > 1 {
+            let guess = match graph.guess {
+                Some(v) => v,
+                None => suggest(&GUESSES, &remaining_ans).to_owned(),
+            };
+
+            // everytime an outcome is generated, increment the tries by one
+            let out = outcome(&guess, &fixed_answer);
+            tries += 1;
+
+            // direct hit on correct answer
+            if out == 242 {
+                return (tries, guess);
+            }
+
+            // shrink answer space
+            remaining_ans.retain(|&answer| outcome(&guess, &answer) == out);
+
+            // save past decisions
+            graph = graph.push(guess, out);
+            guesses.push(st(&guess).to_string());
+        }
+
+        // At this point, remaining_ans should have one left inside
+        // This takes one last try to get it
+        //
+        debug_assert!(remaining_ans.len() == 1);
+        (tries + 1, remaining_ans[0])
+    }
+
+    pub fn bench(&self, print_count: usize) -> (f64, Duration) {
+        let mut total_tries = 0;
+        let n = self.answers.len();
+        let printerval = n / print_count;
+        let mut graph = Node::new();
+
+        let start = Instant::now();
+        let mut prev = start;
+
+        for i in 0..n {
+            if i % printerval == 0 && i > 0 {
+                println!("{i}/{n} ({:?})", Instant::elapsed(&prev));
+                prev = Instant::now();
+            }
+            let (tries, generated_answer) = self.solve_one(i, &mut graph);
+            debug_assert_eq!(st(&self.answers[i]), st(&generated_answer));
+            total_tries += tries;
+        }
+        (total_tries as f64 / n as f64, Instant::elapsed(&start))
+    }
 }
 
 fn main() {
-    let all_answers = words::ANSWERS;
-    let len = all_answers.len();
-    let mut graph = Node::new();
-    let mut total_tries = 0u32;
+    let wordle = Wordle::new(words::guesses(), words::answers());
+    let (avg_tries, time) = wordle.bench(10);
 
-    let intervals = 10;
-    let printerval = all_answers.len() / intervals;
-
-    let start = Instant::now();
-    let mut prev = start;
-
-    for i in 0..len {
-        if i % printerval == 0 && i > 0 {
-            println!("{i}/{} ({:?})", all_answers.len(), Instant::elapsed(&prev));
-            prev = Instant::now();
-        }
-        let (tries, generated_answer) = solve(&all_answers, i, &mut graph);
-        debug_assert_eq!(st(all_answers[i]), st(&generated_answer));
-        total_tries += tries;
-    }
-    let avg = total_tries as f64 / all_answers.len() as f64;
-
-    println!("time elapsed: {:?}", Instant::elapsed(&start));
-    println!("avg tries: {avg}",)
+    println!("time elapsed: {:?}", time);
+    println!("avg tries: {avg_tries}")
 }
