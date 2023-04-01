86d1161bfa34ed37d9983bd85d9dcfd452ed2f93
nguyenvukhang <brew4k@gmail.com>
2023-04-01 14:22:20 +0800

    revert: restore suggest() method

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,41 +1,14 @@
 use std::collections::HashMap;
+use std::time::Instant;
 
+use crate::entropy::Entropy;
 use crate::types::{outcome_str, Outcome, Word};
 use crate::util::{outcome, st};
 use crate::words::GUESSES;
 
-struct EntropyCache {
-    db: HashMap<usize, HashMap<Vec<usize>, f64>>,
-}
-
-impl EntropyCache {
-    fn new() -> Self {
-        Self { db: HashMap::new() }
-    }
-
-    fn set(&mut self, guess: usize, answers: &Vec<usize>, entropy: f64) {
-        let ht = match self.db.get_mut(&guess) {
-            Some(v) => v,
-            None => {
-                self.db.insert(guess, HashMap::new());
-                self.db.get_mut(&guess).unwrap()
-            }
-        };
-        ht.insert(answers.clone(), entropy);
-    }
-
-    fn get(&self, guess: usize, answers: &Vec<usize>) -> Option<f64> {
-        self.db
-            .get(&guess)
-            .map(|ht| ht.get(answers))
-            .flatten()
-            .map(|v| *v)
-    }
-}
-
 pub struct Matrix {
     db: Vec<Vec<Outcome>>,
-    entropy_cache: EntropyCache,
+    entropy: Entropy,
 }
 
 fn vec<F: Fn(usize) -> T, T>(len: usize, f: F) -> Vec<T> {
@@ -62,7 +35,7 @@ impl Matrix {
                 .iter()
                 .map(|a| guesses.iter().map(|g| outcome(g, a)).collect())
                 .collect(),
-            entropy_cache: EntropyCache::new(),
+            entropy: Entropy::new(answers.len()),
         }
     }
 
@@ -70,21 +43,19 @@ impl Matrix {
         self.db[answer][guess]
     }
 
-    pub fn entropy_cached(&mut self, guess: usize, answers: &Vec<usize>) -> f64 {
-        if let Some(v) = self.entropy_cache.get(guess, answers) {
-            return v;
-        }
-        let entropy = self.entropy(guess, answers);
-        self.entropy_cache.set(guess, answers, entropy);
-        entropy
-    }
-
     pub fn entropy(&mut self, guess: usize, answers: &Vec<usize>) -> f64 {
         let mut freq = [0; 243];
         for answer in answers {
             freq[self.outcome(guess, *answer) as usize] += 1;
         }
-        build_entropy(freq, answers.len())
+        let mut entropy = 0.0;
+        let n = answers.len();
+        for f in freq {
+            if f > 0 {
+                entropy += self.entropy.get(f, n);
+            }
+        }
+        entropy
     }
 
     fn out_freq(&self, guess: usize, answers: &Vec<usize>) -> Vec<usize> {
@@ -99,82 +70,61 @@ impl Matrix {
     }
 
     pub fn entropy2(&mut self, guess: usize, answers: &Vec<usize>) -> f64 {
+        let start = Instant::now();
         let (g1, n) = (guess, self.guess_count());
         let out_freq = self.out_freq(g1, answers);
         let mut entropy2 = 0.0;
+
         for o1 in 0..243 {
             // `guess` will never lead to this outcome.
             // example: guess is "iiiii" gives outcome GGGGG.
-            if out_freq[o1 as usize] == 0 {
+            if out_freq[o1] == 0 {
                 continue;
             }
 
             // `guess` + outcome `o1` -> results in this answer list
-            let answers = self.shrink(g1, o1, answers);
+            let answers = self.shrink(g1, o1 as Outcome, answers);
+
+            let mut all_freqs: [usize; 1024] = [0; 1024];
 
             for g2 in 0..n {
                 if g1 == g2 {
                     continue;
                 }
-                let ent2 = self.entropy(g2, &answers);
-                entropy2 += ent2 * out_freq[o1 as usize] as f64;
+                let mut freq = [0; 243];
+                for a in &answers {
+                    freq[self.outcome(g2, *a) as usize] += 1;
+                }
+                freq.into_iter().for_each(|f| all_freqs[f] += 1);
             }
 
-            println!("`{}` -> {}:", st(GUESSES[guess]), outcome_str(o1));
+            let mut temp_ent = 0.0;
+            for f in 0..all_freqs.len() {
+                if all_freqs[f] == 0 {
+                    continue;
+                }
+                temp_ent += all_freqs[f] as f64 * self.entropy.get(f, answers.len());
+            }
+            entropy2 += out_freq[o1] as f64 * temp_ent;
+
+            println!("{}->{}:", st(GUESSES[guess]), outcome_str(o1 as Outcome));
         }
         entropy2 /= (n * n) as f64;
         println!("{g1} -> 2nd: {}", entropy2);
+        println!("elapsed: {:?}", Instant::elapsed(&start) * n as u32);
         entropy2
     }
 
-    pub fn probe_next(&mut self, guess: usize, answers: &Vec<usize>) {
-        let g1 = guess;
-        let n = self.guess_count();
-        let mut total_2nd_ent = 0.0;
-        let mut outcome1_freq = vec(243, |_| 0);
-        for answer in answers {
-            outcome1_freq[self.outcome(g1, *answer) as usize] += 1;
-        }
-        for o1 in 0..243 {
-            // this outcome is not possible given the guess.
-            // example: guess is "zzzzz" (which is not a word) and
-            // outcome is GGGGG. No such answer exists.
-            if outcome1_freq[o1 as usize] == 0 {
-                continue;
-            }
-
-            // suppose the outcome of `out` occurred.
-            // this will be the state of the answers list
-            //
-            // guess1 and outcome1 uniquely define this state.
-            let mut answers = answers.clone();
-            answers.retain(|ans| self.outcome(g1, *ans) == o1);
-
-            // THIS is the bottleneck
-            // n = 13,000 so it gets really expensive at this level
-            for g2 in 0..n {
-                if g1 == g2 {
-                    continue;
-                }
-                let ent2 = self.entropy(g2, &answers);
-                total_2nd_ent += ent2 * outcome1_freq[o1 as usize] as f64;
+    pub fn suggest(&self, answers: &Vec<usize>) -> usize {
+        let mut best = (0, -1.0);
+        let guess_count = self.guess_count();
+        for guess in 0..guess_count {
+            let entropy = self.entropy(guess, answers);
+            if entropy > best.1 {
+                best = (guess, entropy);
             }
         }
-        total_2nd_ent /= (n * n) as f64;
-        println!("{g1} -> 2nd: {}", total_2nd_ent);
-    }
-
-    pub fn suggest(&mut self, answers: &Vec<usize>) -> usize {
-        let n = self.guess_count();
-
-        // List of (f64, f64), entropies of first and second guess
-        let mut entropies = Vec::with_capacity(n);
-        (0..n).for_each(|g1| entropies.push((self.entropy(g1, answers), 0.0)));
-
-        for g1 in 0..n {
-            self.probe_next(g1, answers);
-        }
-        0
+        best.0
     }
 
     pub fn answer_count(&self) -> usize {
