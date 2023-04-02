44ad66860695bf7c7aea788a9a58170157a5e147
nguyenvukhang <brew4k@gmail.com>
2023-04-02 11:44:20 +0800

    feat: add generation for cpp database

--- cpp/main.cc
+++ cpp/main.cc
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
   std::cout << "hello" << std::endl;
-  for (auto i : guesses) {
+  for (auto i : GUESSES) {
     std::cout << i << std::endl;
   }
   return 0;
--- rust/words/generate.js
+++ rust/words/generate.js
@@ -2,29 +2,58 @@ const { spawnSync } = require("child_process");
 const { readFileSync, writeFileSync, rmSync } = require("fs");
 const { join, relative } = require("path");
 
-const TARGET_DIR = join(__dirname, "../src/words");
+const targetDir = {
+  rust: join(__dirname, "../src/words"),
+  cpp: join(__dirname, "../../cpp"),
+};
 
-const build = {};
-
-/**
- * @param {string} filename
- */
-build.rust = (filename) => {
-  const words = readFileSync(filename, "utf8").trim().split("\n");
-  const content =
-    `pub const ${filename.toUpperCase()}: [&[u8; 5]; ${words.length}] = [` +
-    words.map((v) => `b"${v}"`).join(",") +
-    "];";
-  const filepath = join(TARGET_DIR, filename + ".rs");
+const readFile = (f) => readFileSync(f, "utf8").trim().split("\n");
+function writeFile(filepath, content, fmt = () => {}) {
   rmSync(filepath, { recursive: true, force: true });
   writeFileSync(filepath, content);
   console.log(`Generated ${relative(__dirname, filepath)}!`);
   try {
-    spawnSync("rustfmt", [filepath], { cwd: TARGET_DIR });
+    fmt();
   } catch {}
+}
+
+const build = {};
+const GUESSES = readFile("guesses");
+const ANSWERS = readFile("answers");
+
+/**
+ * @param {string} filename
+ */
+build.rust = (filename, words) => {
+  const content =
+    `pub const ${filename.toUpperCase()}: [&[u8; 5]; ${words.length}] = [` +
+    words.map((v) => `b"${v}"`).join(",") +
+    "];";
+  const filepath = join(targetDir.rust, filename + ".rs");
+  const fmt = () => spawnSync("rustfmt", [filepath], { cwd: targetDir.rust });
+  writeFile(filepath, content, fmt);
 };
 
-build.cpp = () => {};
+build.cpp = () => {
+  const frame = (guesses, answers) => `\
+#ifndef WORDLE_WORDS_H
+#define WORDLE_WORDS_H
 
-build.rust("guesses");
-build.rust("answers");
+#include <string>
+#include <vector>
+
+const std::vector<std::string> GUESSES{${guesses}};
+const std::vector<std::string> ANSWERS{${answers}};
+
+#endif`;
+  let [guesses, answers] = [GUESSES, ANSWERS].map(JSON.stringify);
+  guesses = guesses.replace("[", "").replace("]", "");
+  answers = answers.replace("[", "").replace("]", "");
+  const content = frame(guesses, answers);
+  const filepath = join(targetDir.cpp, "words.h");
+  writeFile(filepath, content);
+};
+
+build.rust("guesses", GUESSES);
+build.rust("answers", ANSWERS);
+build.cpp();
